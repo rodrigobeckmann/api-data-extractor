4036d400a5c054dd4f997e99aa4c68af4df58d87
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2025-01-25 16:17:01 -0300

    feat: added base client for api client implementation

---
+++ api_data_extractor/client.py
@@ -0,0 +1,70 @@
+import requests
+from urllib.parse import urljoin
+
+
+class ApiClient:
+
+    def __init__(
+        self,
+        url: str,
+        headers: dict = None,
+    ):
+        self.url = url
+        self.headers = headers
+
+    @staticmethod
+    def _handle_response(
+        response: requests.Response,
+    ):
+        if not response.ok:
+            raise Exception(f"Error: {response.status_code}: {response.text}")
+
+    def _make_url(
+        self,
+        path: str,
+    ) -> str:
+        return urljoin(self.url, path)
+
+    def _request(
+        self,
+        method: str,
+        data: dict = None,
+        params: dict = None,
+    ) -> requests.Response:
+        response = requests.request(
+            method,
+            self.url,
+            headers=self.headers,
+            data=data,
+            params=params,
+        )
+        self._handle_response(response)
+        return response
+
+    def get(
+        self,
+        path: str,
+        params: dict = None,
+    ) -> requests.Response:
+        response = self._request(
+            url=self._make_url(path),
+            method='GET',
+            params=params,
+        )
+        return response
+
+    def post(
+        self,
+        path: str,
+        params: dict = None,
+        data: dict = None,
+        json: dict = None,
+    ) -> requests.Response:
+        response = self._request(
+            url=self._make_url(path),
+            method='POST',
+            params=params,
+            data=data,
+            json=json,
+        )
+        return response
